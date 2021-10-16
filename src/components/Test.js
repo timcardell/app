5e80d27816bc93b8a25611245f7116be35fca15a
Tim Cardell <timcardell@Tims-MBP.lan>
2021-10-16 14:43:07 +0200

    stuff

---
+++ src/components/Test.js
@@ -0,0 +1,9 @@
+function Test() {
+    return (
+      <div className="App">
+        <p> hej</p>
+      </div>
+    );
+  }
+  
+  export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Test from './components/Test'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Test></Test>
   </React.StrictMode>,
   document.getElementById('root')
 );
